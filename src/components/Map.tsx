75b17b508cdd04c091668d81579c3a7263f3a15a
DanielRiveraA279 <danielriveraruta3@gmail.com>
2022-02-22 10:08:57 -0300

    Polyline

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
+import React, { useState, useEffect, useRef } from 'react';
+import MapView, { PROVIDER_GOOGLE, Marker, Polyline } from 'react-native-maps'
 import { useLocation } from '../hooks/useLocation';
 import { LoadingScreen } from '../pages/LoadingScreen';
 import { Fab } from './Fab';
@@ -10,13 +10,45 @@ interface Props {
 
 export const Map = ({ markers }: Props) => {
 
-    const { hasLocation, initialPosition, getCurrentLocation } = useLocation();
+    const [showPolyline, setShowPolyline] = useState(true);
+
+    const {
+        hasLocation,
+        initialPosition,
+        getCurrentLocation,
+        followuserLocation,
+        userLocation,
+        stopFollowuserLocation,
+        routeLines } = useLocation();
 
     const mapViewRef = useRef<MapView>();
+    const following = useRef<boolean>(true);
+
+    useEffect(() => {
+        followuserLocation(); //posicion actual del usuario
+        return () => {
+            stopFollowuserLocation()
+        }
+    }, []);
+
+    //escuchar cambios y seguir con la camara del gps
+    useEffect(() => {
+
+        const { latitude, longitude } = userLocation;
+
+        if (!following.current) return; //si ya no esta siguiendo que no haga nada
+
+        mapViewRef.current?.animateCamera({
+            center: { latitude, longitude }
+        })
+
+    }, [userLocation]);
 
     const centerPosition = async () => {
 
         const { latitude, longitude } = await getCurrentLocation();
+
+        following.current = true; //para volver a darle el seguimiento al usuario
 
         mapViewRef.current?.animateCamera({
             center: {
@@ -37,21 +69,36 @@ export const Map = ({ markers }: Props) => {
                 ref={(el) => mapViewRef.current = el!}
                 style={{ flex: 1 }}
                 // provider={PROVIDER_GOOGLE} Para apple
+                showsUserLocation
                 region={{
                     latitude: initialPosition.latitude,
                     longitude: initialPosition.longitude,
                     latitudeDelta: 0.015,
                     longitudeDelta: 0.0121,
                 }}
+                //cuando arrastro el mapa que no
+                onTouchStart={() => following.current = false}
             >
+
+                {showPolyline && (
+                    <Polyline
+                        coordinates={routeLines}
+                        strokeColor="red"
+                        strokeWidth={3}
+                    />
+                )}
+
                 {/* <Marker
                     image={require('../assets/custom-marker.png')}
                     coordinate={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
+                        latitude: initialPosition.latitude,
+                        longitude: initialPosition.longitude,
                     }}
                     title='Esto es un titulo'
                     description='Esto es una descripcion del marcador'
+                    style={{
+                        width: 20
+                    }}
                 /> */}
             </MapView>
 
@@ -61,7 +108,16 @@ export const Map = ({ markers }: Props) => {
                 style={{
                     position: 'absolute',
                     bottom: 10,
-                    right: 10,
+                    right: 20,
+                }}
+            />
+             <Fab
+                iconName='brush-outline'
+                onPress={() => setShowPolyline(value => !value)}
+                style={{
+                    position: 'absolute',
+                    bottom: 80,
+                    right: 20,
                 }}
             />
         </>
--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -1,25 +1,35 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Geolocation from '@react-native-community/geolocation';
 import { Location } from '../interface/appInterface';
-
 
 export const useLocation = () => {
 
     const [hasLocation, setHasLocation] = useState(false);
+    const [routeLines, setRouteLines] = useState<Location[]>([]);
+
     const [initialPosition, setInitialPosition] = useState<Location>({
         longitude: 0,
         latitude: 0
     });
 
+    const [userLocation, setUserLocation] = useState<Location>({
+        longitude: 0,
+        latitude: 0
+    });
+
+    const watchId = useRef<number>();
+
     useEffect(() => {
         getCurrentLocation()
             .then(location => {
                 setInitialPosition(location);
+                setUserLocation(location);
+                setRouteLines(routes => [...routes, location]); //almacenamos localizacion actual y agregamos nueva localizacion
                 setHasLocation(true);
             })
-    }, []); 
+    }, []);
 
-    //promesa que resuelve resultados de tipo location (interface)
+    //promesa que resuelve resultados de tipo location (interface) - Centrar la camara del Mapa
     const getCurrentLocation = (): Promise<Location> => {
         return new Promise((resolve, reject) => {
             //regresa una promesa, con la posicion actual del usuario
@@ -36,9 +46,37 @@ export const useLocation = () => {
 
     }
 
+    //Seguir al usuario
+    const followuserLocation = () => {
+        //obtengo numeros para limpiar el seguimiento de la camara gps
+        watchId.current = Geolocation.watchPosition(
+            ({ coords }) => {
+
+                const location: Location = {
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                }
+
+                setUserLocation(location);
+                setRouteLines(routes => [...routes, location])
+            },
+            (err) => console.log(err), { enableHighAccuracy: true /*para usar el gps*/, distanceFilter: 10 /*cad vez que pase 10 metros me va a notificars*/ },
+        );
+    }
+
+    const stopFollowuserLocation = () => {
+        //si tiene un valor llama al clearWatch(watchId)
+        if (watchId.current)
+            Geolocation.clearWatch(watchId.current);
+    }
+
     return {
         hasLocation,
         initialPosition,
-        getCurrentLocation
+        getCurrentLocation,
+        followuserLocation,
+        userLocation,
+        stopFollowuserLocation,
+        routeLines
     }
 }
